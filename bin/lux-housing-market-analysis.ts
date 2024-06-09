d046482cf6312b27283e60bbfb30442fd317338a
Omar <ouardighiomar@gmail.com>
2024-06-09 11:31:20 +0200

    .

--- bin/lux-housing-market-analysis.ts
+++ bin/lux-housing-market-analysis.ts
@@ -3,7 +3,10 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { DataIngestionStack } from '../lib/data-ingestion-stack';
 import { TrustStack } from '../lib/trust-stack';
+import { GlueCdkStack } from '../lib/glue-stack';
 
 const app = new cdk.App();
-new DataIngestionStack(app, 'DataIngestionStack', {});
+
 new TrustStack(app, 'TrustStack', {});
+new DataIngestionStack(app, 'DataIngestionStack', {});
+new GlueCdkStack(app, 'GlueCdkStack', {});
